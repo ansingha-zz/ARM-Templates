2fbc12e8b1b7953443f8c016b110996e6233f0a8
Ankit Singhal <ankit.singhal@outlook.com>
2017-06-18 15:53:52 +0530

    Add files via upload

--- Install - Configure WSUS/deploy.ps1
+++ Install - Configure WSUS/deploy.ps1
@@ -3,70 +3,14 @@
     Provision a VM with WSUS role
 
 .DESCRIPTION
-    Provisions a VM , adds a 500 GB data disk and installs WSUS role
-
-.Parameter $subscriptionId
-    Provide the Azure Subscription Id where WSUS VM will be deployed
-  
-.Parameter $Region
-    Provide Region where WSUS VM will be deployed
-
-.Parameter $ExistingVirtualNetworkName
-    Provide the existing Virtual Network names
-
-.Parameter $customScriptFileToRun
-    Example: InstallWSUS.ps1
-
-.Parameter $customScriptStorageAccountName
-    Provide the storage account name where the custom script file is stored
-
-.Parameter $customScriptStorageAccountNamesResourceGroup
-    Provide the Resource Groups Names of the storage accounts where custom scripts are uploaded
-
-.Parameter $keyVaultName
-    Provide the Key Vault name that exists in the other subscription
-
-.Parameter $managementSubscriptionId
-    Provide the Azure Subscription Id for the Key Vault
-
-.Parameter $CSEContainerName
-    Provide the Container name where CustomScriptExtension scripts are uploaded
-
-.PARAMETER $Tags
-    Tag values for Resource Group
-
-.Parameter $ResourceGroupName
-    Provide name of the existing Resource Group which have the existing VNets
-
-.Parameter $VMStorageAccountName
-	Name of Azure Storage Account associated with the VM
-
-.Parameter $VSTSDeploymentPath
-    Provide the VSTS Deployment Path for JSON templates
-
-.Parameter $VSTSDeploymentName
-    Provide the Name for Template Deployment
-
-.Parameter $SyncHours
-    The schedule for the daily sync
-
-.Parameter $SyncMinutes
-    The schedule for the daily sync
-
-.Parameter $workspaceName
-    OMS Workspace
-
-.Parameter $workspaceResourceGroup
-    OMS Workspace Resource Group
-)
+    Provision a VM, adds a 500 GB data disk and installs and configures WSUS role. It also adds the OMS extension to the VM.
 #>
 
 param(
 [Parameter(Mandatory=$True, HelpMessage="Provide the Azure Subscription Id where WSUS VM will be deployed")]
 [string]
 $subscriptionId,
-  
-[validateset("EN","UW" )]
+
 [Parameter(Mandatory=$true,  HelpMessage="Provide Region where WSUS VM will be deployed")]
 [string]
 $Region,
@@ -87,13 +31,13 @@ $customScriptStorageAccountName,
 [string]
 $customScriptStorageAccountNamesResourceGroup,
 
+[Parameter(Mandatory=$True, HelpMessage="Provide the Azure Subscription Id for the Key Vault (the Management Subscription)")]
+[string]
+$managementSubscriptionId,
+
 [Parameter(Mandatory=$True, HelpMessage="Provide the Key Vault name that exists in the other subscription")]
 [string]
 $keyVaultName,
-
-[Parameter(Mandatory=$True, HelpMessage="Provide the Azure Subscription Id for the Key Vault")]
-[string]
-$managementSubscriptionId,
 
 [Parameter(Mandatory=$True, HelpMessage="Provide the Container name where CustomScriptExtension script is uploaded")]
 [string]
@@ -103,15 +47,15 @@ $CSEContainerName,
 [PSCustomObject]
 $Tags,
 
-[Parameter(Mandatory=$True, HelpMessage="Provide name of the existing Resource Group which have the existing VNets
+[Parameter(Mandatory=$True, HelpMessage="Provide name of the existing Resource Group which have the existing VNets")]
 [string]
 $ResourceGroupName,
 
-[Parameter(Mandatory=$True, HelpMessage="Provide the Name of Azure Storage Account associated with the VM")]
+[Parameter(Mandatory=$True, HelpMessage="Provide the Name of Azure Storage Account to be associated with the VM")]
 [string]
 $VMStorageAccountName,
 
-[Parameter(Mandatory=$True, HelpMessage="Provide the VSTS Deployment Path for JSON templates")]
+[Parameter(Mandatory=$True, HelpMessage="Provide the Deployment Path for JSON templates")]
 [string]
 $VSTSDeploymentPath,
 
@@ -144,7 +88,7 @@ $workspaceResourceGroup
 $ErrorActionPreference = "Stop"
 
 # Select Key Vault subscription
-Log-Information "Selecting Management Subscription: '$managementSubscriptionId'";
+Write-Output "Selecting Management Subscription: '$managementSubscriptionId'";
 Set-AzureRmContext -SubscriptionId $managementSubscriptionId
 
 
@@ -165,18 +109,14 @@ $workspaceKey = $key.PrimarySharedKey
 
 
 # Select subscription to create the VM
-Log-Information "Selecting the subscription to create the VM: '$subscriptionId'";
+Write-Output "Selecting the subscription to create the VM: '$subscriptionId'";
 Set-AzureRmContext -SubscriptionID $subscriptionId;
-    
-    $Location = ConvertFrom-RegionCode $Region
-
-Ensure-ResourceGroup -ResourceGroupName $ResourceGroupName -Location $Location -Tags $Tags
 
 #Deploy the Template to the Resource Group
-Log-Information "Deploying to $Location in $ResourceGroupName"
+Write-output "Deploying to $Region in $ResourceGroupName"
 
-
-$parameters = @{"AdminUsername"=$AdminUsername.SecretValue; "AdminPassword"=$AdminPassword.SecretValue; "region"=$region; "existingVirtualNetworkName"=$existingVirtualNetworkName; "customScriptFileToRun"=$customScriptFileToRun; "customScriptStorageAccountName"=$customScriptStorageAccountName; "SASToken"="$SASToken"; "VMStorageAccountName"=$VMStorageAccountName; "SyncHours"=$SyncHours; "SyncMinutes"=$SyncMinutes; "workspaceName"=$workspaceName; "workspaceId"=$workspaceId; "workspaceKey"=$workspaceKey }    
+$parameters = @{"AdminUsername"=$AdminUsername.SecretValue; "AdminPassword"=$AdminPassword.SecretValue; "existingVirtualNetworkName"=$existingVirtualNetworkName; "customScriptFileToRun"=$customScriptFileToRun; "customScriptStorageAccountName"=$customScriptStorageAccountName; "SASToken"="$SASToken"; "VMStorageAccountName"=$VMStorageAccountName; "SyncHours"=$SyncHours; "SyncMinutes"=$SyncMinutes; "workspaceName"=$workspaceName; "workspaceId"=$workspaceId; "workspaceKey"=$workspaceKey }    
 $null = New-AzureRmResourceGroupDeployment -Name $VSTSDeploymentName -ResourceGroupName $ResourceGroupName -TemplateFile "$VSTSDeploymentPath\azuredeploy.json" -TemplateParameterObject $parameters -Mode Incremental
-Log-Information "Deployed to $Location in $ResourceGroupName"
-Log-Information "Deployment is done !"
+
+Write-output "Deployed to $Region in $ResourceGroupName"
+Write-output "Deployment is done !"
